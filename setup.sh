37f1cec14bed588c31884657f764116b936fb297
Olek Puchka <proxtreem@gmail.com>
2025-06-20 22:00:07 +0200

    feat: remove cyberduck

--- setup.sh
+++ setup.sh
@@ -56,7 +56,6 @@ brew install zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 brew install --cask visual-studio-code
 brew install --cask postman
-brew install --cask cyberduck
 
 ###############################################################################
 # Communication Apps                                                          #
